063a8b18ce629d8702b75980ba03c2344b1793ac
Luong Cong Dan <lcd11001@gmail.com>
2020-09-11 14:01:38 +0700

    + image

--- SharedComponents/DynamicRouter.js
+++ SharedComponents/DynamicRouter.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 function GetLink(props) {
     return (
         <div>
+            <div className={props.className}></div>
             <Link href={`/SecondPage?content=${props.title}`} as={props.shortUrl}>
                 <a>{props.title}</a>
             </Link>
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,40 @@ import GetLink from '../SharedComponents/DynamicRouter'
 function App() {
     return (
         <div>
+            <style jsx global>
+                {`
+                    a {
+                        color: blue;
+                    }
+                    .img {
+                        width: 64px;
+                        height: 64px;
+                        background-size: cover !important;
+                        background-repeat: no-repeat !important;
+                        background-position: center !important;
+                        border: 1px solid black;
+                        border-radius: 10px;
+                        display: inline-block;
+                        margin-top: 10px;
+                    }
+                    .p1 {
+                        background: url(/Icons/angry.png)
+                    }
+                    .p2 {
+                        background: url(/Icons/happy.png)
+                    }
+                    .p3 {
+                        background: url(/Icons/love.png)
+                    }
+                `}
+            </style>
             <p>Hello from Next.js</p>
             <Link href='/about'>
                 <a>About</a>
             </Link>
-            <GetLink title={'Page 1'} shortUrl={'page-1'} />
-            <GetLink title={'Page 2'} shortUrl={'page-3'} />
-            <GetLink title={'Page 3'} shortUrl={'page-3'} />
+            <GetLink title={'Page 1'} shortUrl={'page-1'} className={'img p1'} />
+            <GetLink title={'Page 2'} shortUrl={'page-3'} className={'img p2'} />
+            <GetLink title={'Page 3'} shortUrl={'page-3'} className={'img p3'} />
         </div>
     )
 }
