f63bcbff761f0f9c4887c74cff3a70ac6084bb02
Luong Cong Dan <lcd11001@gmail.com>
2020-09-11 16:12:42 +0700

    + redux

---
+++ pages/ReduxDemo.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../style.css'
+
+export default class extends React.Component {
+    static async getInitialProps() {
+        return {
+            text: 'Initial label value'
+        }
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: props.text
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Redux Demo</h1>
+                <br />
+                <div className={'center'}>
+                    <input id='inputTextbox' type='text' />
+                    <button type='button' onClick={this.GetUser}>
+                        Update label
+                    </button>
+                </div>
+                <br />
+                <p>{this.state.text}</p>
+            </div>
+        )
+    }
+}
